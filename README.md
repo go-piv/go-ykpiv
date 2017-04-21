ec7ecf9ecdd1cab197c80c9f821c7154a67f5e61
Paul Tagliamomnte <tag@pault.ag>
2017-04-21 18:18:20 -0400

    Initial import of the ykpiv wrapper

---
+++ README.md
@@ -0,0 +1,83 @@
+go-ykpiv
+========
+
+go-ykpiv is a high level cgo wrapper around `libykpiv.so.1` that implements an
+idiomatic go API fit for use when applications need to communicate with a
+Yubikey in PIV mode.
+
+What's PIV?
+-----------
+
+PIV Cards are cards defined by FIPS 201, a Federal US Government standard
+defining the ID cards employees use. At its core, it's a set of x509
+Certificates and corresponding private keys in a configuration that is
+standardized accross implementations.
+
+For more details on how PIV Tokens can be used, the FICAM
+(Federal Identity, Credential, and Access Management) team at GSA
+(General Services Administration) has published some guides on GitHub
+under [GSA/piv-guides](https://github.com/GSA/piv-guides)
+
+How is this different than OpenSC?
+----------------------------------
+
+Most PIV tokens, Yubikeys included, can be used as a PKCS#11 device using
+[OpenSC](https://github.com/opensc/opensc), and Yubikeys are even capable of doing
+Signing and Decryption through that interface. However, some management functions
+are not exposed in the PKCS#11 OpenSC interface, so this library may be of use
+when one wants to write a new Certificate, or set PINs.
+
+Examples
+========
+
+```go
+package main
+
+import (
+	"fmt"
+
+	"pault.ag/go/ykpiv"
+)
+
+func main() {
+	yubikey, err := ykpiv.New(ykpiv.Options{
+		// Verbose: true,
+		Reader: "Yubico Yubikey NEO U2F+CCID 01 00",
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer yubikey.Close()
+
+	version, err := yubikey.Version()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Application version %s found.\n", version)
+}
+```
+
+LICENSE
+=======
+
+```
+Copyright (c) Paul R. Tagliamonte <paultag@gmail.com>, 2017
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+```
---
+++ ykpiv.go
@@ -0,0 +1,153 @@
+// {{{ Copyright (c) Paul R. Tagliamonte <paultag@gmail.com>, 2017
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE. }}}
+
+package ykpiv
+
+/*
+#cgo LDFLAGS: -lykpiv
+#cgo CFLAGS: -I/usr/include/ykpiv/
+#include <ykpiv.h>
+#include <stdlib.h>
+*/
+import "C"
+
+import (
+	"fmt"
+	"unsafe"
+
+	"crypto/x509"
+)
+
+// Configuration for initialization of the Yubikey, as well as options that
+// may be used during runtime.
+type Options struct {
+
+	// When true, this will cause the underlying ykpiv library to emit additional
+	// information to stderr. This can be helpful when debugging why something
+	// isn't working as expected.
+	Verbose bool
+
+	// String to be used when searching for a Yubikey. The comparison
+	// will be against the output you can observe from
+	// `yubico-piv-tool -a list-readers`.
+	Reader string
+}
+
+// Encapsulation of the ykpiv internal state object, and the configuration
+// in new. This needs to be initalized through `ykpiv.New` to ensure the
+// internal state is brought up correctly.
+//
+// This object represents a single physical yubikey that we've connected to.
+// This object provides a number of helper functions hanging off the struct
+// to avoid keeping and passing the internal ykpiv state object in C.
+//
+// `.Close()` must be called, or this will leak memory.
+type Yubikey struct {
+	state   *C.ykpiv_state
+	options Options
+}
+
+// Close the Yubikey object, and preform any finization needed to avoid leaking
+// memory or holding locks.
+func (y Yubikey) Close() error {
+	if C.ykpiv_disconnect(y.state) != C.YKPIV_OK {
+		return fmt.Errorf("ykpiv: ykpiv_disconnect Failed to disconnect the reader.")
+	}
+
+	if C.ykpiv_done(y.state) != C.YKPIV_OK {
+		return fmt.Errorf("ykpiv: ykpiv_done Failed to close state.")
+	}
+
+	// This will free the underlying state, no need to C.free the object by
+	// hand here
+
+	return nil
+}
+
+// Return the ykpiv application version. This is expected to be in the format of
+// '1.2.3', but is up to the underlying ykpiv application code.
+func (y Yubikey) Version() ([]byte, error) {
+	var versionLength = C.size_t(7)
+
+	var version unsafe.Pointer = C.malloc(versionLength)
+	defer C.free(version)
+
+	if C.ykpiv_get_version(y.state, (*C.char)(version), versionLength) != C.YKPIV_OK {
+		return nil, fmt.Errorf("ykpiv: ykpiv_get_version Failed to retrieve application version.")
+	}
+
+	return C.GoBytes(version, C.int(versionLength)), nil
+}
+
+//
+func (y Yubikey) Certificate(slot Slot) (*x509.Certificate, error) {
+	var dataLen C.ulong = 3072
+	var data *C.uchar = (*C.uchar)(C.malloc(3072))
+	defer C.free(unsafe.Pointer(data))
+
+	if C.ykpiv_fetch_object(y.state, C.int(slot), data, &dataLen) != C.YKPIV_OK {
+		return nil, fmt.Errorf("ykpiv: ykpiv_fetch_object Failed fetching certificate.")
+	}
+
+	// some magic shit going down here. I'm not exactly sure what. This needs
+	// a metric fuckload of testing. There's some sort of length encoding
+	// in the underlying string. My guess is the DER that I've got back
+	// falls into the same general length and never triggered some voodoo
+	// with dynamic length byte prefixes. There's a p. good chance this is
+	// just outright wrong.
+	der := C.GoBytes(unsafe.Pointer(data), C.int(dataLen))[4 : dataLen-5]
+
+	// If this is throwing sequence truncated and/or trailing byte errors
+	// the first thing to double check is the byte mangling above, and
+	// if the comment above applies to this.
+	return x509.ParseCertificate(der)
+}
+
+// Create a new Yubikey.
+//
+// This will use the options in the given `ykpiv.Options` struct to
+// find the correct Yubikey, initialize the underlying state, and ensure
+// the right bits are set.
+func New(opts Options) (*Yubikey, error) {
+	yubikey := Yubikey{
+		state:   &C.ykpiv_state{},
+		options: opts,
+	}
+
+	verbosity := 0
+	if opts.Verbose {
+		verbosity = 1
+	}
+
+	if C.ykpiv_init(&yubikey.state, C.int(verbosity)) != C.YKPIV_OK {
+		return nil, fmt.Errorf("ykpiv: ykpiv_init Failed to connect to reader.")
+	}
+
+	something := C.CString(opts.Reader)
+	defer C.free(unsafe.Pointer(something))
+
+	if C.ykpiv_connect(yubikey.state, something) != C.YKPIV_OK {
+		return nil, fmt.Errorf("ykpiv: ykpiv_connect Failed to connect to reader.")
+	}
+
+	return &yubikey, nil
+}
+
+// vim: foldmethod=marker
