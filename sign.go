74047a07d9fb51a69ae9e6d96563d7e9784f2ee0
Paul Tagliamomnte <tag@pault.ag>
2017-04-22 18:59:37 -0400

    Add in Signing
    This does a bit of nasty stuff, including what amounts to a homebrew
    implementation of PKCS#1 1.5 padding.

---
+++ sign.go
@@ -0,0 +1,105 @@
+// {{{ Copyright (c) Paul R. Tagliamonte <paultag@gmail.com>, 2017
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE. }}}
+
+package ykpiv
+
+/*
+#cgo LDFLAGS: -lykpiv
+#cgo CFLAGS: -I/usr/include/ykpiv/
+#include <ykpiv.h>
+#include <stdlib.h>
+*/
+import "C"
+
+import (
+	"fmt"
+	"io"
+	"unsafe"
+
+	"crypto"
+)
+
+// It's never a real party until you import both `unsafe`, *and* `crypto`.
+
+var hashOIDs = map[crypto.Hash][]byte{
+	crypto.SHA224: {0x30, 0x2d, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x04, 0x05, 0x00, 0x04, 0x1c},
+	crypto.SHA256: {0x30, 0x31, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01, 0x05, 0x00, 0x04, 0x20},
+	crypto.SHA384: {0x30, 0x41, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x02, 0x05, 0x00, 0x04, 0x30},
+	crypto.SHA512: {0x30, 0x51, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x03, 0x05, 0x00, 0x04, 0x40},
+}
+
+func prepareDigestForRSA256(hash crypto.Hash, digest []byte) []byte {
+	outputLength := 256
+
+	padding := make([]byte, (outputLength - 3 - len(digest)))
+	for i := 0; i < len(padding); i++ {
+		padding[i] = 0xFF
+	}
+
+	return expandBytes([]byte{0x00, 0x01}, padding, []byte{0x00}, digest)
+}
+
+func expandBytes(els ...[]byte) []byte {
+	out := []byte{}
+	for _, el := range els {
+		out = append(out, el...)
+	}
+	return out
+}
+
+func (y Yubikey) Sign(rand io.Reader, digest []byte, opts crypto.SignerOpts) ([]byte, error) {
+	// XXX: yank the C.YKPIV_ALGO_RSA2048 out and replace it with a real check
+	// on what the slot is under the hood.
+
+	hash := opts.HashFunc()
+	if len(digest) != hash.Size() {
+		return nil, fmt.Errorf("ykpiv: Sign: Digest length doesn't match passed crypto algorithm")
+	}
+
+	prefix, ok := hashOIDs[hash]
+	if !ok {
+		return nil, fmt.Errorf("ykpiv: rsa digest: Unsupported algorithm")
+	}
+	digest = append(prefix, digest...)
+
+	computedDigest := prepareDigestForRSA256(hash, digest)
+
+	var cDigest = (*C.uchar)(C.CBytes(computedDigest))
+	var cDigestLen = C.size_t(len(computedDigest))
+	defer C.free(unsafe.Pointer(cDigest))
+
+	var cSignatureLen = C.size_t(1024)
+	var cSignature = (*C.uchar)(C.malloc(cSignatureLen))
+
+	if err := getError(C.ykpiv_sign_data(
+		y.state,
+		cDigest, cDigestLen,
+		cSignature, &cSignatureLen,
+
+		C.YKPIV_ALGO_RSA2048,
+		C.uchar(0x9a),
+	), "sign_data"); err != nil {
+		return nil, err
+	}
+
+	return C.GoBytes(unsafe.Pointer(cSignature), C.int(cSignatureLen)), nil
+}
+
+// vim: foldmethod=marker
