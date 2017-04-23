8a29d7906fd03a20112893e7414629d21ff33fd7
Paul Tagliamomnte <tag@pault.ag>
2017-04-22 20:52:16 -0400

    Update docstrings

--- slot.go
+++ slot.go
@@ -37,36 +37,54 @@ import (
 	"unsafe"
 )
 
-// Slot ID (0x9a, etc) {{{
-
+// SlotId encapsulates the Identifiers required to preform key operations
+// on the Yubikey. The identifier most people would know (if this is a thing
+// that people do who don't write PIV aware applications) would be the `Key`
+// Id, something like 0x9A.
 type SlotId struct {
 	Certificate int32
 	Key         int32
 }
 
+// Return a human readable string mostly useful for debugging which Slot you
+// might have your hands on. Since most people (for some value of "most")
+// would want the Key, this only includes that.
 func (s SlotId) String() string {
 	return fmt.Sprintf("Slot key=%x", s.Key)
 }
 
+// More information regarding the basic PIV slots can be founnd at the
+// FICAM piv-gude: https://piv.idmanagement.gov/elements/
 var (
+	// PIV Authentication, which is a certificate and key pair and can be used
+	// to verify that the PIV credential was issued by an authorized entity,
+	// has not expired, has not been revoked, and holder of the credential
+	// (YOU) is the same individual it was issued to.
 	Authentication SlotId = SlotId{
 		Certificate: C.YKPIV_OBJ_AUTHENTICATION,
 		Key:         C.YKPIV_KEY_AUTHENTICATION,
 	}
 )
 
-// }}}
-
+// Slot abstracts a public key, private key, and x509 Certificate stored
+// on the PIV device.
+//
+// Internally, this keeps track of the Yubikey this came from, the underlying
+// object identifiers for the Certificate and Key we care about, as well as
+// other bits and bobs of state.
 type Slot struct {
 	yubikey     Yubikey
 	id          SlotId
 	certificate x509.Certificate
 }
 
+// Get the PIV Authentication Slot off the Yubikey. This is identical to
+// invoking `yubikey.Slot(ykpiv.Authentication)`.
 func (y Yubikey) Authentication() (*Slot, error) {
 	return y.Slot(Authentication)
 }
 
+// Get a Slot off of the Yubikey by the SlotId.
 func (y Yubikey) Slot(id SlotId) (*Slot, error) {
 	/* Right, let's see what we can do here */
 	slot := Slot{yubikey: y, id: id}
@@ -79,14 +97,18 @@ func (y Yubikey) Slot(id SlotId) (*Slot, error) {
 	return &slot, nil
 }
 
+// Return the crypto.PublicKey that we know corresponds to the Certificate
+// we have on hand.
 func (s Slot) Public() crypto.PublicKey {
 	return s.certificate.PublicKey
 }
 
+// Get the SlotId for the current Slot
 func (s Slot) Id() SlotId {
 	return s.id
 }
 
+// Get the x509.Certificate stored in the PIV Slot.
 func (y Slot) Certificate() (*x509.Certificate, error) {
 	var dataLen C.ulong = 3072
 	var data *C.uchar = (*C.uchar)(C.malloc(3072))
